ebcc5a9d67a4d9f8ac3d1d9e0bce79bc459ba125
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-09-25 11:05:12 -0500

    Create Class Service

---
+++ BarbeShop/BarbeShop/DAL/Entities/Service.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BarbeShop.DAL.Entities
+{
+    public class Service : Entity
+    {
+        [Display(Name = "Servicio")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] //LONGITUD MÁXIMA (NVARCHAR(50))
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
+        public string Name { get; set; }
+
+        [Display(Name = "Descripción")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] //LONGITUD MÁXIMA (NVARCHAR(50))
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
+        public string Description { get; set; }
+    }
+}
