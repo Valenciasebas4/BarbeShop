65cefae43abc2556112540f7afe0f5b02955f7c6
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-09-25 11:21:12 -0500

    The connection chain to the database is entered

--- BarbeShop/BarbeShop/DAL/DataBaseContext.cs
+++ BarbeShop/BarbeShop/DAL/DataBaseContext.cs
@@ -1,6 +1,26 @@
-﻿namespace BarbeShop.DAL
+﻿using BarbeShop.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection.Emit;
+
+namespace BarbeShop.DAL
 {
-    public class DataBaseContext
+    public class DataBaseContext : DbContext
     {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Service> Services { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Service>().HasIndex(c => c.Name).IsUnique();
+
+            
+        }
     }
 }
