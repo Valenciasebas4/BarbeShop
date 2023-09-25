404a553df0a852dfc59530ff969468359e5685d0
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-09-25 11:00:57 -0500

    Create folder DAL-Entities and class Entity

---
+++ BarbeShop/BarbeShop/DAL/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarbeShop.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
